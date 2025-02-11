630e466f19f08190100b7dd73db97184165641f4
Hansel Omondi <ogopabhs@gmail.com>
2025-02-11 19:16:02 +0300

    Add: kotlin-serialization plugin
    The kotlinx-serialization-json dependency has also been added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -48,6 +49,7 @@ dependencies {
     implementation(libs.bundles.compose)
     implementation(libs.bundles.networking)
     implementation(libs.bundles.koin)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

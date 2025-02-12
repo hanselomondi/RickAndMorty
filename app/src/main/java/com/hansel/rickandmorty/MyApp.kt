1ac2a4970193ccb0d2c26655d761154af81652e8
Hansel Omondi <ogopabhs@gmail.com>
2025-02-12 22:00:42 +0300

    feat: register Retrofit instance in Koin modules
    The Koin modules have been registered in the Application class under the
    startKoin function. Moreover, the Application class has been registered
    in the AndroidManifest

---
+++ app/src/main/java/com/hansel/rickandmorty/MyApp.kt
@@ -0,0 +1,21 @@
+package com.hansel.rickandmorty
+
+import android.app.Application
+import com.hansel.rickandmorty.di.apiServiceModule
+import com.hansel.rickandmorty.di.okHttpModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApp)
+            modules(
+                okHttpModule + apiServiceModule
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/hansel/rickandmorty/di/AppModules.kt
@@ -0,0 +1,44 @@
+package com.hansel.rickandmorty.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.hansel.rickandmorty.data.remote.RickAndMortyApi
+import com.hansel.rickandmorty.util.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val okHttpModule = module {
+    single {
+        OkHttpClient.Builder()
+            .readTimeout(20, TimeUnit.SECONDS)
+            .callTimeout(35, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+}
+
+val apiServiceModule = module {
+    single<Gson> {
+        GsonBuilder()
+            .setLenient()
+            .create()
+    }
+
+    single {
+        Retrofit.Builder()
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create(get<Gson>()))
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    single {
+        get<Retrofit>().create(RickAndMortyApi::class.java)
+    }
+}

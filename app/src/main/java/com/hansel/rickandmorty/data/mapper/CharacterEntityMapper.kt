113fc41b5a71c9bb9758b92cfde30df017057652
Hansel Omondi <ogopabhs@gmail.com>
2025-02-25 02:01:54 +0300

    feat: local caching of fetched characters

---
+++ app/src/main/java/com/hansel/rickandmorty/data/mapper/CharacterEntityMapper.kt
@@ -0,0 +1,30 @@
+package com.hansel.rickandmorty.data.mapper
+
+import com.hansel.rickandmorty.data.local.CharacterEntity
+import com.hansel.rickandmorty.domain.model.Character
+
+fun CharacterEntity.toDomainCharacter(): Character {
+    return Character(
+        id = id,
+        name = name,
+        status = status,
+        species = species,
+        type = type,
+        location = location,
+        image = image,
+        episodes = episodes
+    )
+}
+
+fun Character.toCharacterEntity(): CharacterEntity {
+    return CharacterEntity(
+        id = id,
+        name = name,
+        status = status,
+        species = species,
+        type = type,
+        location = location,
+        image = image,
+        episodes = episodes
+    )
+}
--- app/src/main/java/com/hansel/rickandmorty/domain/repository/CharacterRepository.kt
+++ app/src/main/java/com/hansel/rickandmorty/domain/repository/CharacterRepository.kt
@@ -2,9 +2,10 @@ package com.hansel.rickandmorty.domain.repository
 
 import com.hansel.rickandmorty.domain.model.Character
 import com.hansel.rickandmorty.domain.model.NetworkResult
+import kotlinx.coroutines.flow.Flow
 
 interface CharacterRepository {
-    suspend fun getCharacters(): NetworkResult<List<Character>>
+    suspend fun getCharacters(): Flow<NetworkResult<List<Character>>>
 
     suspend fun getCharacterById(id: Int): NetworkResult<Character>
 }

59e62129366ae515ed86d117b20eded7f7051f0c
Hansel Omondi <ogopabhs@gmail.com>
2025-02-24 21:33:11 +0300

    Add: CharacterEntity with table name "characters"
    A Room Entity representing the characters table

---
+++ app/src/main/java/com/hansel/rickandmorty/data/local/CharacterEntiity.kt
@@ -0,0 +1,19 @@
+package com.hansel.rickandmorty.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hansel.rickandmorty.domain.model.Character
+import com.hansel.rickandmorty.domain.model.CharacterStatus
+
+@Entity(tableName = "characters")
+data class CharacterEntity(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val status: CharacterStatus,
+    val species: String,
+    val type: String?,
+    val location: Character.CharacterLocation,
+    val image: String,
+    val episodes: List<String>
+)

bb67d9a00f6d981f924f35a678d9115fa93e3ac6
Hansel Omondi <ogopabhs@gmail.com>
2025-03-04 01:05:30 +0300

    Add: FontScale and FormFactors to CustomPreview

--- app/src/main/java/com/hansel/rickandmorty/util/CustomPreview.kt
+++ app/src/main/java/com/hansel/rickandmorty/util/CustomPreview.kt
@@ -4,6 +4,35 @@ import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
+    name = "small font",
+    group = "fonts",
+    fontScale = 0.5f
+)
+@Preview(
+    name = "large font",
+    group = "fonts",
+    fontScale = 1.5f
+)
+annotation class FontScalePreviews
+
+@Preview(
+    name = "phone",
+    group = "devices",
+    device = "spec:width=360dp,height=640dp,dpi=480"
+)
+@Preview(
+    name = "phone",
+    group = "devices",
+    device = "spec:width=673dp,height=841dp,dpi=480"
+)
+@Preview(
+    name = "phone",
+    group = "devices",
+    device = "spec:width=1280dp,height=800dp,dpi=480"
+)
+annotation class DevicePreviews
+
+@Preview(
     name = "LightThemePreview",
     showBackground = true,
     uiMode = Configuration.UI_MODE_NIGHT_NO
@@ -13,4 +42,6 @@ import androidx.compose.ui.tooling.preview.Preview
     showBackground = true,
     uiMode = Configuration.UI_MODE_NIGHT_YES
 )
+@FontScalePreviews
+@DevicePreviews
 annotation class CustomPreview
